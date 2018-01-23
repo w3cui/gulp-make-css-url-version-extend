23a9c53ea952c4af6738d54df9e6bc18c6c02640
tanqilin <tanqilin@xiaochankeji.cn>
2018-01-23 15:32:55 +0800

    项目托管

---
+++ README.md
@@ -0,0 +1,91 @@
+## gulp-make-css-url-version-extend 是在 gulp-make-css-url-version-s 基础上做了bug修复版本
+
+
+<p>修改了 gulp-make-css-url-version 增加版本号的方式：</p>
+<p>原来的日期模式是"yy-mm-dd" 修改成以毫秒为序列号</p>
+<p>原来的MD5模式引用字段过长引起CSS文件过大，现在截取前8位为版本号</p>
+<p>解决图片url以域名 （"http://", "https://", "//"）为前缀导致找不到文件路径而无法修改版本号问题；</p>
+<p>可过滤不需要修改版本号的路径</p>
+
+## 安装
+
+```bash
+npm install gulp-make-css-url-version-extend
+```
+
+## 使用
+默认MD5模式
+```js
+var makeUrlVer = require('gulp-make-css-url-version-extend');
+
+gulp.task('stylesheets', function() {
+    gulp.src('css/*.css')
+        .pipe(makeUrlVer())
+        .pipe(gulp.dest('dist'))
+});
+```
+
+## 配置
+
+<p>使用日期模式：</p>
+
+```js
+var makeUrlVer = require('gulp-make-css-url-version-extend');
+
+gulp.task('stylesheets', function() {
+    gulp.src('css/*.css')
+    //原格式：yy-mm-dd 修改后格式：201711061728(时间拼接精确到毫秒)  
+        .pipe(makeUrlVer({useDate:true}))  
+        .pipe(gulp.dest('dist'))
+});
+```
+##### 域名路径配置
+```js
+var makeUrlVer = require('gulp-make-css-url-version-extend');
+gulp.task('stylesheets', function() {
+    gulp.src('css/*.css')
+        .pipe(cssver({
+            /*域名替换，路径必须以'/'结束 ;
+              例如" http://abc.com/images/logo.png" 用'../'替换后你可以处理的路径是'../images/logo.png' */
+            domainName:'../',  
+            //过滤不需要加版本号的域名 
+            excludeDomain:["//abc.cn/","http://abc.com/"]}))
+        .pipe(gulp.dest('dist'))
+});
+
+```
+assetsDir: specify the public directory for correct MD5 calculation in some specific cases
+
+```js
+var makeUrlVer = require('gulp-make-css-url-version-extend');
+
+gulp.task('stylesheets', function() {
+    gulp.src('css/*.css')
+        .pipe(makeUrlVer({
+            assetsDir: __dirname + '/public'
+        }))
+        .pipe(gulp.dest('dist'))
+});
+```
+
+## Example
+
+### before: index.css
+
+```css
+/* loading */
+.i-loading{background:url(../images/loading.gif) ;} 
+.logo{background:url(//js.abc.com/images/loading.png) ;}    
+   
+```
+
+### after: index.css
+
+```css
+/* loading */
+.i-loading{background:url(../images/loading.gif?v=Je0sUcMH)}
+.logo{background:url(//js.abc.com/images/logo.png?v=Je0sUcMH) ;}    
+```
+ 
+
+ 
---
+++ index.js
@@ -0,0 +1,149 @@
+
+var path = require('path');
+var crypto = require('crypto');
+var fs = require('fs');
+
+var gutil = require('gulp-util');
+var through = require('through2');
+var Q = require('q');
+var ndate = new Date()
+var PLUGIN_NAME = 'gulp-make-css-url-version';
+
+var getMD5 = function (data) {
+    var hash = crypto.createHash("md5");
+    hash.update(data);
+    var md5Base64 = hash.digest("base64");
+    return md5Base64;
+};
+
+var formatDate = function (format, date) {
+    date = date || new Date();
+    var o = {
+        "M+": date.getMonth() + 1,
+        "d+": date.getDate(),
+        "h+": date.getHours(),
+        "m+": date.getMinutes(),
+        "s+": date.getSeconds(),
+        "q+": Math.floor((date.getMonth() + 3) / 3),
+        "S": date.getMilliseconds()
+    };
+    format = format.replace(/y{4}/, date.getFullYear()).replace(/y{2}/, date.getFullYear().toString().substring(2));
+
+    for (var k in o) {
+        var reg = new RegExp(k);
+        format = format.replace(reg, match);
+    }
+    function match(m) {
+        return m.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length);
+    }
+    return format;
+};
+module.exports = function (options) {
+    return through.obj(
+        function (file, enc, cb) {
+            options = options || {};
+            var self = this;
+
+            var fileName = file.path.split(path.sep).pop();
+
+            var excludeDomain = options.excludeDomain
+            var newArr = [];
+            //  过滤配置URL
+            function fillterDomain() {
+                for (var i = 0; i < excludeDomain.length; i++) {
+                    newArr.push(`url.indexOf("${excludeDomain[i]}")`)
+                }
+                return newArr.join(">-1 || ").replace(/^".*"$/)
+            }
+            if (file.isNull()) {
+                this.push(file);
+                return cb();
+            }
+            if (file.isStream()) {
+                this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+                return cb();
+            }
+            //css file only
+            if (!/^\.css?$/.test(path.extname(file.path))) {
+                gutil.log(gutil.colors.red('[WARN] file ' + fileName + ' is not a css file'));
+                this.push(file);
+                return cb();
+            }
+
+            var html = file.contents.toString();
+            var promises = [];
+            html = html.replace(/url\("?([^\)"]+)"?\)/g, function (str, url, oldurl) {
+
+                //url = url.replace(/\?[\s\S]*$/, "").trim(); //font
+                oldurl = url.replace(/['"]*/g, "");
+
+                var testUrl = "url.indexOf('base64,') > -1 || url.indexOf('about:blank') > -1  || url === '/' ||" + fillterDomain() + ">-1";
+                if (eval(testUrl)) {
+                    return str;
+                }
+                console.log(testUrl)
+                if (options.domainName) {
+                    url = url.replace(/['"]*/g, "").replace(/^https:\/\/.*?\//, options.domainName);
+                    url = url.replace(/['"]*/g, "").replace(/^http:\/\/.*?\//, options.domainName);
+                    url = url.replace(/['"]*/g, "").replace(/^\/\/.*?\//, options.domainName);
+                }
+
+                var format = options.format || "" + ndate.getFullYear() + ndate.getMonth() + ndate.getDate() + ndate.getMinutes() + ndate.getSeconds() + ndate.getMilliseconds();
+                //use date as the version
+                if (options.useDate) {
+                    return "url(" + oldurl + "?v=" + formatDate(format, new Date()) + ")";
+                }
+                //use md5
+                var safeUrl = url.replace(/#[\s\S]*$/, '');
+                var imageFilePath;
+                if (options.assetsDir) {
+                    imageFilePath = path.join(options.assetsDir, safeUrl);
+                } else {
+                    imageFilePath = path.resolve(path.dirname(file.path), safeUrl);
+                }
+
+                var tempKey = Math.random().toString();
+                var readFile = Q.denodeify(fs.readFile);
+
+                var promise = readFile(imageFilePath)
+                    .then(function (data) {
+                        //gutil.log('replacing image ' + imageFilePath + ' version in css file: ' + file.path);
+                        var verStr = data ? getMD5(data.toString()).substr(0, 8) : formatDate(format);
+                        return {
+
+                            key: tempKey,
+                            value: "url(" + oldurl + "?v=" + verStr + ")"
+                        };
+                    }, function (e) {
+                        gutil.log(e);
+                        return {
+                            key: tempKey,
+                            value: "url(" + oldurl + "?v=" + formatDate(format) + ")"
+                        }; 
+                    });
+ 
+                promises.push(promise);
+
+                return tempKey;
+            });
+
+            if (options.useDate) {
+                file.contents = new Buffer(html);
+                self.push(file);
+                cb();
+                return;
+            }
+
+            Q.all(promises).then(function (mathces) {
+
+                mathces.forEach(function (match) {
+                    html = html.replace(match.key, match.value);
+                });
+
+                file.contents = new Buffer(html);
+                self.push(file);
+                cb();
+            });
+
+        });
+};
